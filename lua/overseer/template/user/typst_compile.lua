2efa815665b60fc268ee4f9a6ee345721568e914
lcdse7en <2353442022@qq.com>
2024-07-18 17:17:12 +0800

    add

--- lua/overseer/template/user/typst_compile.lua
+++ lua/overseer/template/user/typst_compile.lua
@@ -7,6 +7,7 @@ return {
 		local file_dir = vim.fn.expand("%:p:h")
 		local root_dir = vim.fn.fnamemodify(file_dir, ":h")
 		local font_dir = root_dir .. "/fonts"
+		local output_file = vim.fn.expand("%:p:r") .. ".pdf"
 		-- local project_root = vim.fn.finddir(".git", file_dir .. ";")
 		-- if project_root == "" then
 		-- 	project_root = cwd
@@ -17,13 +18,20 @@ return {
 			name = vim.fn.expand("%:t"),
 			cwd = file_dir,
 			cmd = { "typst" },
-			args = { "compile", vim.fn.expand("%:p"), "--root", root_dir, "--font-path", font_dir },
+			args = { "compile", vim.fn.expand("%:p"), output_file, "--root", root_dir, "--font-path", font_dir },
 			conponents = {
 				"task_list_on_start",
 				"display_duration",
 				"on_exit_set_status",
 				"on_complete_notify",
 			},
+			on_exit = function(_, return_val)
+				if return_val == 0 then
+					vim.api.nvim_command("silent !zathura " .. output_file .. " &")
+				else
+					print("Typst compilation failed.")
+				end
+			end,
 		}
 	end,
 }
