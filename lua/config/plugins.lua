1a070a9685cb1801d063e38f2a98803e563d29ef
lcdse7en <2353442022@qq.com>
2024-07-30 12:16:32 +0800

    add

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1346,4 +1346,19 @@ return {
 			end)
 		end,
 	},
+	{
+		"mg979/vim-visual-multi",
+		keys = {
+			"<C-n>",
+			"<C-Up>",
+			"<C-Down>",
+			"<S-Up>",
+			"<S-Down>",
+			"<S-Left>",
+			"<S-Right>",
+		},
+		config = function()
+			vim.g.VM_theme = "codedark"
+		end,
+	},
 }
