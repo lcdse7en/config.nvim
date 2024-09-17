934c33c640c71d0fb80635f96a45cb5d929f24cd
lcdse7en <2353442022@qq.com>
2024-09-17 19:35:10 +0800

    add

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -566,6 +566,7 @@ return {
     build = 'cd app && yarn install',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
+      vim.keymap.set('n', '<leader>mp', "<cmd>MarkdownPreview<CR>", { desc = 'MarkdownPreview' })
     end,
   },
   {
