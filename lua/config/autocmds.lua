2917d9c91711334d64bb096942ad656f0961449d
lcdse7en <2353442022@qq.com>
2024-10-11 10:39:53 +0800

    add

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -507,16 +507,10 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
-vim.api.nvim_create_autocmd(
-    {
-        "BufNewFile",
-        "BufRead",
-    },
-    {
-        pattern = "*.typ",
-        callback = function()
-            local buf = vim.api.nvim_get_current_buf()
-            vim.api.nvim_buf_set_option(buf, "filetype", "typst")
-        end
-    }
-)
+vim.api.nvim_create_autocmd({"BufNewFile","BufRead"},{
+  pattern = "*.typ",
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_option(buf, "filetype", "typst")
+  end
+})
