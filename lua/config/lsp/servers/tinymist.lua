6780b96396b2ee66d7356df0f301181da72803f1
lcdse7en <2353442022@qq.com>
2024-10-11 12:49:49 +0800

    add

--- lua/config/lsp/servers/tinymist.lua
+++ lua/config/lsp/servers/tinymist.lua
@@ -2,8 +2,16 @@ local M = {}
 
 M.settings = {
   tinymist = {
-    exportPdf = "onType",
-    outputPath = "$root/target/$dir/$name",
+      exportPdf = "onType",
+      outputPath = "$root/target/$dir/$name",
+    -- single_file_support = true,
+    -- root_dir = function ()
+    --   return vim.fn.getcwd()
+    -- end,
+    -- settings = {
+    --   exportPdf = "onType",
+    --   outputPath = "$root/target/$dir/$name",
+    -- }
   }
 }
 
